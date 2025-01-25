0f8b4eac695897a392526640707613c07e632220
Paul Kalinin <51087816+pashkalini@users.noreply.github.com>
2025-01-25 15:31:12 +0300

    Update giga_am_30_sec.py

--- giga_am_30_sec.py
+++ giga_am_30_sec.py
@@ -1,9 +1,13 @@
 # Базовое использование - распознование речи на коротких аудиозаписях (до 30 секунд)
 from GigaAM import gigaam
-
+#
 # model = gigaam.load_model("ctc")
 # print(model)
 
-model_name = "rnnt"  # Options: "v2_ctc" or "ctc", "v2_rnnt" or "rnnt", "v1_ctc", "v1_rnnt"
-model = gigaam.load_model(model_name)
-transcription = model.transcribe('converted_audio/output_1.wav')
+
+def giga_transcript_30_sec(audio):
+    model_name = "ctc"  # Options: "v2_ctc" or "ctc", "v2_rnnt" or "rnnt", "v1_ctc", "v1_rnnt"
+    model = gigaam.load_model(model_name)
+    transcription = model.transcribe(audio)
+
+    return transcription
