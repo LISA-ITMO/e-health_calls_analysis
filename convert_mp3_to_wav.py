be39f93f2e0513c06427ded29d7610635d02a548
Paul Kalinin <51087816+pashkalini@users.noreply.github.com>
2025-04-05 19:38:55 +0300

    Update convert_mp3_to_wav.py

--- convert_mp3_to_wav.py
+++ convert_mp3_to_wav.py
@@ -1,13 +1,28 @@
 from pydub import AudioSegment
+import glob
 
-# Specify input MP3 and output WAV file paths
-mp3_file = file
-wav_file = file_1
 
-# Load the MP3 file
-audio = AudioSegment.from_mp3(mp3_file)
+def convert_audio(mp3_path, wav_path):
+    # Specify input MP3 and output WAV file paths
+    mp3_file = mp3_path
+    wav_file = wav_path
 
-# Export as WAV file
-audio.export(wav_file, format="wav")
+    # Load the MP3 file
+    audio = AudioSegment.from_mp3(mp3_file)
 
-print(f"Converted {mp3_file} to {wav_file}")
+    # Export as WAV file
+    audio.export(wav_file, format="wav")
+
+    print(f"Converted {mp3_file} to {wav_file}")
+
+
+def convert_all(dirname):
+    # Указываем директорию, в которой нужно искать файлы
+
+    i = 1
+    for audio in glob.glob(f'{dirname}/**/*.mp3', recursive=False):
+        # Формируем путь для выходного WAV файла
+        wav_path = f"converted_audio/output_{i}.wav"
+        i += 1
+        # Вызываем функцию для конвертации и записи файла
+        convert_audio(audio, wav_path)
