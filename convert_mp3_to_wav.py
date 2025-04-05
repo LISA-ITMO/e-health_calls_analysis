5c019cb25b845eae735e95b025d7b7e76dc47f33
Paul Kalinin <51087816+pashkalini@users.noreply.github.com>
2025-04-05 19:12:28 +0300

    Update convert_mp3_to_wav.py

--- convert_mp3_to_wav.py
+++ convert_mp3_to_wav.py
@@ -1,8 +1,8 @@
 from pydub import AudioSegment
 
 # Specify input MP3 and output WAV file paths
-mp3_file = ("B:\YandexDisk\Теперь тут\ИТМО\Аспирантура\Данные для анализа\Ленинградская область\ЗАПИСИ 122 ЛО\отобранные записи\/79523774950_Групповой_78136269882_2024_06_10_100455.mp3")
-wav_file = "B:\YandexDisk\DISK\ITMO\Aspirantura\Code\Sber_Giga_recognition\giga_recognition\converted_audio\output_2.wav"
+mp3_file = file
+wav_file = file_1
 
 # Load the MP3 file
 audio = AudioSegment.from_mp3(mp3_file)
